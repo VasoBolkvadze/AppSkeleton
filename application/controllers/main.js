d9c4eb723cc80718b8c764006040eb617dabfffc
Vaso Bolkvadze <vaso.bolkvadze@gmail.com>
2016-07-13 18:34:22 +0400

    v2

--- application/controllers/main.js
+++ application/controllers/main.js
@@ -1,6 +1,6 @@
 module.exports = function (router) {
 	router.get('/'
 		, function (req, res, next) {
-			res.render('home/default',{title:'Satcitananda!'});
+			res.render('home/default',{title:'express yourself!'});
 		});
 };
